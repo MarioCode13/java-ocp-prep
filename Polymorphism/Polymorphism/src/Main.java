f5f4fb49ec412bfe3851c9123441d5d2d61f4a2e
MarioCode13 <mario.liebenberg@hotmail.com>
2025-11-23 00:01:47 +0200

    polymorphism

---
+++ Polymorphism/Polymorphism/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Movie theMovie = new Adventure("Star Wars");
+        theMovie.watchMovie();
+
+        Movie theMovie2 = new Comedy("Grown Ups");
+        theMovie2.watchMovie();
+    }
+}
---
+++ Polymorphism/Polymorphism/src/Movie.java
@@ -0,0 +1,72 @@
+public class Movie {
+
+    String title;
+
+    public Movie(String title) {
+        this.title = title;
+    }
+
+    public void watchMovie() {
+            describeMovie();
+        String instanceType = this.getClass().getSimpleName(); 
+    }
+    
+    public void describeMovie() {
+        System.out.println(title + " is a " + this.getClass().getSimpleName());
+    }
+}
+
+class Adventure extends Movie {
+
+    public Adventure(String title) {
+        super(title);
+    }
+
+    @Override
+    public void describeMovie() {
+
+        System.out.println(this.title + " is an " + this.getClass().getSimpleName());
+    }
+    
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3),
+                "Pleasant Scene",
+                "Scary Music",
+                "Something Bad Happens");
+    }
+    
+}
+
+class Comedy extends Movie {
+
+    public Comedy(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3),
+                "Something funny happens",
+                "Something even funnier happens",
+                "Happy Ending");
+    }
+}
+
+class ScienceFiction extends Movie {
+
+    public ScienceFiction(String title) {
+        super(title);
+    }
+
+    @Override
+    public void watchMovie() {
+        super.watchMovie();
+        System.out.printf(".. %s%n".repeat(3),
+                "Bad Aliens do Bad Stuff",
+                "Space Guys Chase Aliens",
+                "Planet Blows Up");
+    }
+}
