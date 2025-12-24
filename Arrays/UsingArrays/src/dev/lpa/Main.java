f1084981c4137c4f50226ba17ee2c326d3fa403d
MarioCode13 <mario.liebenberg@hotmail.com>
2025-12-24 13:09:46 +0200

    sorting arrays

--- Arrays/UsingArrays/src/dev/lpa/Main.java
+++ Arrays/UsingArrays/src/dev/lpa/Main.java
@@ -33,6 +33,22 @@ public class Main {
 
         int[] largerArray = Arrays.copyOf(thirdArray, 15);
         System.out.println(Arrays.toString(largerArray));
+
+        String[] sArray = {"Abel", "Jane", "Mark", "Ralph", "David", "Alex"};
+        Arrays.sort(sArray);
+        System.out.println(Arrays.toString(sArray));
+        if(Arrays.binarySearch(sArray, "Abel") >= 0) {
+            System.out.println("Found Abel");
+        }
+
+        int[] s1 = {1,2,3,4,5};
+        int[] s2 = {1,2,3,5,4};
+
+        if(Arrays.equals(s1, s2)) {
+            System.out.println("Same array");
+        } else {
+            System.out.println("Different array");
+        }
     }
 
     private static int[] getRandomArray(int len) {
